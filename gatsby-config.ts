4e91c4001d2b9409089b3bf2f6abb6ed54cde937
Vlad Korolev <vlad@v-lad.org>
2024-04-30 21:20:47 -0400

    Fixup annotations

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -58,9 +58,9 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Vlad's Developer Blog`,
+        short_name: `vlads-blog`,
+        description: `Vlad's Developer Blg`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
